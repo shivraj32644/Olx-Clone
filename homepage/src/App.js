671cf25c336fe7670495a3851566a15e8afa23ab
Suraj <surajybtg@gmail.com>
2022-10-09 18:15:59 +0530

    Navbar added

---
+++ homepage/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
