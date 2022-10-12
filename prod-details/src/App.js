7d578c13f92d627f848ed9afe1d7aea249e5b032
itsvikasdwivedi <vickyserver22@gmail.com>
2022-10-12 11:33:08 +0530

    login-page

---
+++ prod-details/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ControlledCarousel from './Pages/SignIn';
+// import Login from './Pages/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <ControlledCarousel/>
+    </div>
+  );
+}
+
+export default App;
