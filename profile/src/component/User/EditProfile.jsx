dea439c12a1fff668cc4c92c7cbdf63792069bd1
Shakti Modak <shaktimodak81@gmail.com>
2022-10-13 06:36:45 +0530

    solved

--- profile/src/component/User/EditProfile.jsx
+++ profile/src/component/User/EditProfile.jsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { OnError, OnLoading, OnSuccess } from "../../Redux/UserProfile/Action";
-import {CheckIcon,ChevronRightIcon} from "@chakra-ui/icons"
+import { CheckIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { HiOutlineLightBulb } from "react-icons/hi";
 import {
   Box,
   Flex,
   Text,
+  CircularProgress,
   Button,
+  Spinner,
   VStack,
   StackDivider,
   Divider,
@@ -18,12 +21,20 @@ import {
   Textarea,
   InputRightElement,
   InputGroup,
-  InputLeftElement
+  InputLeftElement,
+  Spacer,
+  HStack,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import ProfileDetailForm from "./ProfileDetailFrom";
+import ProfilePicForm from "./ProfilePicForm";
 
 function EditProfile() {
   const { loading, error, user } = useSelector((user) => user);
   const Dispatcher = useDispatch();
+
+  //for windows size paging hook
+  let [profileInDetails, setProfileInDetails] = useState(true);
 
   useEffect(() => {
     getUser();
@@ -49,151 +60,129 @@ function EditProfile() {
       });
   };
 
-  function doClick(e) {
-    e.target.styl.border = "100px";
-  }
+ 
 
-  if (loading) {
-    return <h1>loading..</h1>;
-  }
+ 
 
   if (error) {
     return <h1>error..</h1>;
   }
+  if(loading){
+    return <h1>loading</h1>
+  }
+
+  let TextStyle = {};
 
   return (
-    <Flex width="95%" margin="auto">
-      <Box textAlign={"left"} width="30%" marginRight="20px">
-        <Text>Edit profile</Text>
-        <Text>Profile picture</Text>
+    <Box position={"relative"} height="full">
+      <Flex font-family="'Roboto', sans-serif" width="95%" margin="auto">
+        <Box textAlign={"left"} width="30%" marginRight="20px">
+          {profileInDetails ? (
+            <Text
+              paddingBottom={"8px"}
+              fontWeight="bold"
+              color={"black"}
+              onClick={(e) => {
+                setProfileInDetails(true);
+              }}
+            >
+              Edit profile
+            </Text>
+          ) : (
+            <Text
+              marginBottom={"8px"}
+              color={"gray"}
+              onClick={(e) => {
+                setProfileInDetails(true);
+              }}
+            >
+              Edit profile
+            </Text>
+          )}
+          {profileInDetails ? (
+            <Text
+              color={"gray"}
+              onClick={() => {
+                setProfileInDetails(false);
+              }}
+            >
+              Profile picture
+            </Text>
+          ) : (
+            <Text
+              fontWeight={"bold"}
+              onClick={() => {
+                setProfileInDetails(true);
+              }}
+            >
+              Profile picture
+            </Text>
+          )}
 
-        <Button
-          marginTop={"20px"}
-          size="md"
-          borderRadius={"4px"}
-          height="40px"
-          width="100%"
-          border="2px"
-          color={"black"}
-          borderColor="black"
-          background={"white"}
-          _hover={{ border: "5px solid black", backgroundColor: "white" }}
-        >
-          View profile
-        </Button>
-      </Box>
-
-      <Box
-        width={"full"}
-        variant="outline"
-        border="1px"
-        borderWidth="1px"
-        borderRadius={"md"}
-        borderColor="#cfcdcd"
-      >
-        <Text fontSize="large" textAlign="start" padding={4} fontWeight="bold">
-          Edit profile
-        </Text>
-        <Divider wi />
-
-        <VStack
-          p={5}
-          divider={<StackDivider borderColor="gray.200" />}
-          spacing={4}
-          align="stretch"
-        >
-          <Flex width={"100%"} >
-            <Box width={"100%"} textAlign="left">
-              <FormControl>
-                <FormLabel>Basic information</FormLabel>
-                <Input
-                  width="50%"
-                  _placeholder={{ color: "black" }}
-                  border="1px"
-                  borderRadius="4px"
-                  focusBorderColor={"black"}
-                  placeholder={user.full_name == null ? "Name" : user.full_name}
-                  borderColor="black"
-                />
-                <FormHelperText>This field is mandatory.</FormHelperText>
-              </FormControl>
-
-              <FormControl marginTop={"20px"}>
-              
-                <Textarea
-                  placeholder={user.about_me == null ? "Name" : user.about_me}
-                  border="1px"
-                  borderRadius="4px"
-                  height={"78px"}
-                  p="4px"
-                  focusBorderColor={"black"}
-                  borderColor="black"
-                  width={"50%"}
-                />
-              </FormControl>
-            </Box>
-          </Flex>
-        </VStack>
-
-        <Box padding={5}>
-          <Divider></Divider>
+          <Button
+            marginTop={"20px"}
+            size="md"
+            borderRadius={"4px"}
+            height="40px"
+            width="100%"
+            border="2px"
+            color={"black"}
+            borderColor="black"
+            background={"white"}
+            _hover={{ border: "5px solid black", backgroundColor: "white" }}
+          >
+            View profile
+          </Button>
         </Box>
 
-        <VStack
-          padding={5}
-          divider={<StackDivider borderColor="gray.200" />}
-          spacing={4}
-          align="stretch"
+        <Box
+          width={"full"}
+          variant="outline"
+          border="1px"
+          borderWidth="1px"
+          borderRadius={"4"}
+          borderColor="#cfcdcd"
         >
-          <Flex width={"100%"} >
-            <Box width={"100%"} textAlign="left">
-              <FormControl>
-                <FormLabel>Contact information</FormLabel>
-                <InputGroup width={"50%"} size="lg" >
-                  <InputLeftElement
-                   alignItems={"center"}
-                    pointerEvents="none"
-                    color="gray.300"
-                  
-                    fontSize="1.2em"
-                    children={<Text borderRight={"1px"} px={"10px"} >+91</Text>}
-                   
-                  />
-                  <Input   _placeholder={{ color: "black" }}
-                  border="1px"
-                 paddingLeft={"60px"}
-                  borderRadius="4px"
-                  focusBorderColor={"black"}
-                  placeholder={user.full_name == null ? "Name" : user.full_name}
-                  borderColor="black"/>
-                  <InputRightElement
-                    children={<ChevronRightIcon color="gray.600" fontSize={"30px"} fontWeight="400"/>}
-                  />
-                </InputGroup>
-                
-              </FormControl>
+          <Text fontSize="xl" textAlign="start" padding={4} fontWeight="bold">
+            {profileInDetails ? "Edit profile" : "Profile picture"}
+          </Text>
+          <Divider />
 
-              <FormControl marginTop={"20px"}>
-              <InputGroup width={"50%"}  size="lg" >
-                  
-                  <Input   _placeholder={{ color: "black" } }
-                  border="1px"
-                 type={'email'}
-               
-                  borderRadius="4px"
-                  focusBorderColor={"black"}
-                  placeholder={user.full_name == null ? "Name" : user.full_name}
-                  borderColor="black"/>
-                  <InputRightElement
-                    children={<ChevronRightIcon color="gray.600" fontSize={"30px"} fontWeight="400"/>}
-                  />
-                </InputGroup>
-              </FormControl>
-            </Box>
-          </Flex>
-        </VStack>
-      </Box>
-    </Flex>
+          {/* // for data */}
+          
+
+          {profileInDetails ? (
+           
+            <ProfileDetailForm user={user}  />
+           
+           
+          ) : (
+            <ProfilePicForm user={user} />
+          )}
+        </Box>
+      </Flex>
+
+     { loading &&  <Flex
+        filter="auto"
+        brightness="40%"
+        position={"absolute"}
+        top="0"
+        width="100%"
+        height={"100%"}
+        justifyContent="center"
+        alignItems={"center"}
+        backgroundColor="black"
+        opacity={"0.8"}
+      >
+        <Spinner
+          size="xl"
+          textAlign={"left"}
+          color="white"
+          colorScheme={"white"}
+          thickness="3px"
+        />
+      </Flex> }
+    </Box>
   );
 }
 
---
+++ profile/src/component/User/ProfilePicForm.jsx
@@ -0,0 +1,79 @@
+import { Flex, HStack ,Box, Text,Image,Button, VStack} from "@chakra-ui/react";
+
+function ProfilePicForm({ user }) {
+
+
+
+  
+
+  return (
+    <>
+      <HStack gap={4}  padding={5}>
+        <Image
+        //  border={"2px solid  red"}
+        width={"33%"}
+          borderRadius="full"
+          padding={"20px"}
+         
+          src={user.img_url}
+          alt="User Pic"
+        />
+
+        <Box  height={"fit-content"}   textAlign="left"  width="full">
+           <Text fontSize={"sm"} marginBottom="15px">
+           Clear photos are an important way for buyers and sellers to learn about each other. Be sure doesn’t include any personal or sensitive info you’d rather not have others see.
+           </Text>
+
+           <Text as="b" fontSize={"sm"} >
+           It’s not much fun to chat with a landscape!
+           </Text>
+
+          
+
+          <VStack spacing={2} marginTop="80px">
+
+          <Button
+          marginTop={"20px"}
+          size="md"
+          borderRadius={"4px"}
+          height="40px"
+          width="100%"
+          border="2px"
+          color={"black"}
+          borderColor="black"
+          background={"white"}
+          _hover={{ border: "5px solid black", backgroundColor: "white" }}
+        >
+         Upload
+        </Button>
+
+       <Text color={'green'} fontSize={"sm"}>or</Text>
+
+        <Button
+          marginTop={"20px"}
+          size="md"
+          borderRadius={"4px"}
+          height="40px"
+          width="100%"
+          border="2px"
+          color={"black"}
+          borderColor="black"
+          background={"white"}
+          _hover={{ border: "5px solid black", backgroundColor: "white" }}
+        >
+        Import from Google
+        </Button>
+
+
+          </VStack>
+
+
+        </Box>
+
+
+      </HStack>
+    </>
+  );
+}
+
+export default ProfilePicForm;
