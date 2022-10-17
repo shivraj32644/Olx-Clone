250b615a8762715175e4aa07e434c17e2c42ccef
Suraj <surajybtg@gmail.com>
2022-10-17 21:04:51 +0530

    Navbar Profile

--- homepage/src/Components/Navbar/ProfileBtn.jsx
+++ homepage/src/Components/Navbar/ProfileBtn.jsx
@@ -13,22 +13,37 @@ import {
   Image,
   Menu,
   MenuButton,
+  MenuDivider,
   MenuItem,
   MenuList,
   Text,
+  Divider,
 } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsMenuDown } from "react-icons/bs";
+import { MdContentCopy } from "react-icons/md";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 
 const ProfileBtn = () => {
   return (
     <Box>
       <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Options"
-          icon={<ChevronDownIcon />}
-          variant="outline"
-        />
+        <Flex alignItems={"center"}>
+          <Box width="40px" height="40px" borderRadius="50%">
+            {" "}
+            <Image
+              src="https://statics.olx.in/external/base/img/avatar_3.png"
+              alt="Profile pic"
+            />
+          </Box>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<ChevronDownIcon />}
+          />
+        </Flex>
+
         <MenuList>
           <Flex alignItems="center">
             <Box width="56px" height="56px" borderRadius="50%" mt="15px">
@@ -58,12 +73,15 @@ const ProfileBtn = () => {
               </Text>
             </Box>
           </Flex>
-          <MenuItem icon={<AddIcon />}>
-            <Link to="/">My Adds</Link>
+          <hr />
+          <MenuItem icon={<MdContentCopy />}>
+            <Link to="#">My ADS</Link>
           </MenuItem>
-          <MenuItem icon={<ExternalLinkIcon />}>New Window</MenuItem>
-          <MenuItem icon={<RepeatIcon />}>Open Closed Tab</MenuItem>
-          <MenuItem icon={<EditIcon />}>Open File...</MenuItem>
+          <MenuItem icon={<BsMenuDown />}>Buy Business Packages</MenuItem>
+          <MenuItem icon={<RepeatIcon />}>Open Closed</MenuItem>
+          <MenuItem icon={<RiLogoutBoxRLine />}>
+            <Link to="#">Logout</Link>
+          </MenuItem>
         </MenuList>
       </Menu>
     </Box>
